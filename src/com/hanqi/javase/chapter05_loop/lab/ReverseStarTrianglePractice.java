5344d0add3c5f0245dbcabecba3365495de2cb27
hanqi-code <hanqi20050120@gmail.com>
2026-07-16 13:11:10 +0800

    Chapter05: 完成星号三角形补充练习

---
+++ src/com/hanqi/javase/chapter05_loop/lab/ReverseStarTrianglePractice.java
@@ -0,0 +1,4 @@
+package com.hanqi.javase.chapter05_loop.lab;
+
+public class ReverseStarTrianglePractice {
+}
---
+++ src/com/hanqi/javase/chapter05_loop/lab/StarTrianglePractice.java
@@ -0,0 +1,12 @@
+package com.hanqi.javase.chapter05_loop.lab;
+
+public class StarTrianglePractice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
