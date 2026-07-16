10492143ff303260de27007de5d72cb7491ac219
hanqi-code <hanqi20050120@gmail.com>
2026-07-16 13:11:11 +0800

    Chapter06: 调整数组最小值练习

--- src/com/hanqi/javase/chapter06_array/demo/ArrayMinDemo.java
+++ src/com/hanqi/javase/chapter06_array/demo/ArrayMinDemo.java
@@ -1,4 +1,14 @@
 package com.hanqi.javase.chapter06_array.demo;
 
 public class ArrayMinDemo {
+    public static void main(String[] args) {
+        int[] scores = {90, 85, 76, 88, 95};
+        int min = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            if (min > scores[i]) {
+                min = scores[i];
+            }
+        }
+        System.out.println("最低分是：" + min);
+    }
 }
