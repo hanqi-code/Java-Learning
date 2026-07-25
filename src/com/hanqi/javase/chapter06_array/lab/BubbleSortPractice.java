85ce827d6fc7b19a09abb378dafff35f0e655ee1
hanqi-code <hanqi20050120@gmail.com>
2026-07-25 12:04:54 +0800

    Chapter06: 完成数组冒泡排序练习

---
+++ src/com/hanqi/javase/chapter06_array/lab/BubbleSortPractice.java
@@ -0,0 +1,21 @@
+package com.hanqi.javase.chapter06_array.lab;
+
+public class BubbleSortPractice {
+    public static void main(String[] args) {
+        int[] arr = {50, 20, 40, 10, 30};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                int temp = 0;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
