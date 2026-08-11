014aecbd8a767f3ac671f28a6e808e1938e53061
hanqi-code <hanqi20050120@gmail.com>
2026-08-11 22:45:02 +0800

    Chapter08: 完成成员方法练习

--- src/com/hanqi/javase/chapter08_oop/demo/Student.java
+++ src/com/hanqi/javase/chapter08_oop/demo/Student.java
@@ -4,4 +4,13 @@ public class Student {
     String name;
     int age;
     double score;
+
+    public void showInfo() {
+        System.out.println("姓名：" + name);
+        System.out.println("年龄：" + age);
+        System.out.println("成绩：" + score);
+    }
+    public boolean isPass() {
+        return score >= 60;
+    }
 }
--- src/com/hanqi/javase/chapter08_oop/lab/Phone.java
+++ src/com/hanqi/javase/chapter08_oop/lab/Phone.java
@@ -4,4 +4,14 @@ public class Phone {
     String brand;
     double price;
     String color;
+
+    public void showInfo(){
+        System.out.println("手机品牌：" + brand);
+        System.out.println("手机价格：" + price);
+        System.out.println("手机颜色：" + color);
+    }
+
+    public boolean isExpensive(){
+        return price >= 5000;
+    }
 }
