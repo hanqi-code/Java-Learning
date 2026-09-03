6234ffcebe1124c5e3a2b72f1502503dd9715987
hanqi-code <hanqi20050120@gmail.com>
2026-09-03 14:02:31 +0800

    Chapter10: 完成static final enum章节及综合项目

--- src/com/hanqi/javase/chapter10_static_final_enum/test/GameAccountProject.java
+++ src/com/hanqi/javase/chapter10_static_final_enum/test/GameAccountProject.java
@@ -4,6 +4,7 @@ public class GameAccountProject {
     public static void main(String[] args) {
         GameAccount steve = new GameAccount("USER001", " Steve", 10, AccountStatus.NORMAL);
         GameAccount alex = new GameAccount("USER002", " Alex ", 20, AccountStatus.NORMAL);
+
         System.out.println("===== Initial Accounts =====");
         steve.showInfo();
         alex.showInfo();
